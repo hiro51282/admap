d92b54e3effcb96d32017337c4c8210fd2807fe7
hiro51282 <hiro51282@gmail.com>
2022-07-25 19:00:48 +0900

    最初

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,14 @@ module.exports = {
   parserOptions: {
     parser: '@babel/eslint-parser'
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  overrides: [
+    {
+      rules: {
+        "vue/multi-word-component-names": 0,
+      },
+      files: [
+        "src/views/**/*.vue",
+      ],
+    },
+  ],
 }
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,22 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: [
+    'vuetify'
+  ]
 })
+module.exports = {
+  configureWebpack: {
+    devtool: 'source-map'
+  }
+}
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  publicPath: isProd ? '/リポジトリ名' : '/',
+  outputDir: 'docs',
+  filenameHashing: false,
+  productionSourceMap: false
+}
